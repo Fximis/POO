7ae7ea174c865c0ff999bd0b30dd1a191b2f3aba
Fellipe Schmidt <miquelaoo@gmail.com>
2026-07-30 11:50:17 -0300

    Add files via upload

---
+++ poo-streaming/src/br/com/streaming/model/Categoria.java
@@ -0,0 +1,7 @@
+package br.com.streaming.model;
+
+public class Categoria {
+
+    private String teste;
+    
+}
---
+++ poo-streaming/src/br/com/streaming/model/Video.java
@@ -0,0 +1,8 @@
+package br.com.streaming.model;
+
+public class Video {
+
+    private String titulo;
+    private int duracaoMinutos;
+    private Categoria categoria;
+}
